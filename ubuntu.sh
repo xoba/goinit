07dfd524f65f607a1b47313f54e3014ba2d286d0
mike andrews <mra@xoba.com>
2015-01-19 05:52:23 -0500

    include curl to run the script direct from url

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,4 +2,4 @@
 #
 # prepare a virgin ubuntu box for buildgo.sh
 #
-sudo aptitude update && sudo aptitude install -y libc6-i386 gcc libc6-dev mercurial git libtool make pkg-config emacs
+sudo aptitude update && sudo aptitude install -y curl libc6-i386 gcc libc6-dev mercurial git libtool make pkg-config emacs
