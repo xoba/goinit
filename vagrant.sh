d7df7e32f31be64ddae8d7d617df5a229257d490
mike andrews <mra@xoba.com>
2015-03-03 10:11:36 -0500

    simpler build

--- vagrant.sh
+++ vagrant.sh
@@ -6,7 +6,7 @@ aptitude update && aptitude install -y curl libc6-i386 gcc libc6-dev mercurial g
 /etc/init.d/ntp stop
 ntpd -gq
 /etc/init.d/ntp start
-TGZ=`/vagrant/buildgo.sh`
+/vagrant/buildgo.sh
 if [ $? -eq 0 ]; then
     cp /tmp/go.tar.gz /vagrant/
 fi
