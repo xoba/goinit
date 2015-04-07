e1a3c6e22329f6b6fd5573e3297148dcd02517d9
mike andrews <mra@xoba.com>
2015-04-07 16:30:01 -0400

    cloudinit for ec2

--- ec2-cloudinit.sh
+++ ec2-cloudinit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 aptitude update && aptitude install -y curl libc6-i386 gcc libc6-dev mercurial git libtool make pkg-config emacs ntp
-git clone git@github.com:xoba/goinit.git
+git clone https://github.com/xoba/goinit.git
+cd goinit
+./buildgo.sh
--- ec2.sh
+++ ec2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-ee793a86 --instance-type m3.xlarge --key-name golang_rsa
+aws ec2 run-instances --image-id ami-ee793a86 --instance-type m3.xlarge --key-name golang_rsa --user-data file://ec2-cloudinit.sh
