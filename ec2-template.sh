840b287e661999427a2727b004b932d43db7b6b5
mike andrews <mra@xoba.com>
2015-04-08 11:12:39 -0400

    save log after tar.gz

--- ec2-template.sh
+++ ec2-template.sh
@@ -18,12 +18,12 @@ cd goinit
 
 # upload artifacts:
 cd ~/
-{{ if .s3log }}aws s3 cp log.txt {{.s3log}}
 {{end}}
 {{ if .s3gz }}if [ -f go.tar.gz ]; then
     aws s3 cp go.tar.gz {{.s3gz}}
 fi
 {{end}}
+{{ if .s3log }}aws s3 cp log.txt {{.s3log}}
 
 # wrap-up:
 {{ if .terminate}}aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id`{{end}}
