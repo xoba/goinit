b003ea974e2e9a198540434319ce6a008c7d02cd
mike andrews <mra@xoba.com>
2015-04-08 11:24:48 -0400

    fix up template {{end}}'s

--- ec2-template.sh
+++ ec2-template.sh
@@ -18,12 +18,12 @@ cd goinit
 
 # upload artifacts:
 cd ~/
-{{end}}
 {{ if .s3gz }}if [ -f go.tar.gz ]; then
     aws s3 cp go.tar.gz {{.s3gz}}
 fi
 {{end}}
 {{ if .s3log }}aws s3 cp log.txt {{.s3log}}
+{{end}}
 
 # wrap-up:
 {{ if .terminate}}aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id`
