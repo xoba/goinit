6afc86469a42a72b4623c53a94e2220d83ccc3e8
mike andrews <mra@xoba.com>
2015-04-09 07:47:22 -0400

    get rid of setup comment

--- setupgo.sh
+++ setupgo.sh
@@ -2,7 +2,6 @@
 #
 # sets up go, assumed to be in ~/go -- replaces .emacs!
 #
-# sudo aptitude update && sudo aptitude install -y emacs
 cat >> ~/.bashrc <<EOF
 export GOROOT=~/go
 export PATH=~/go/bin:$PATH
