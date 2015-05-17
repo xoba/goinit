7c82d2581a6116468221de2c915509376af66ab3
mike andrews <mra@xoba.com>
2015-05-17 05:35:40 -0400

    log the ec2 process too

--- cron.sh
+++ cron.sh
@@ -7,5 +7,5 @@ echo "`date` starting $1 $2" >> log.txt
 cd go
 git pull
 cd ..
-source <(./ec2 -profile $1 -gen $2)
+source <(./ec2 -profile $1 -gen $2) 2>&1 | tee -a log.txt
 echo "`date` done $1 $2" >> log.txt
