78904baa2411098a647bd13df9e316e2817a7b39
mike andrews <mra@xoba.com>
2015-05-18 05:49:08 -0400

    add path for aws cli

--- cron.sh
+++ cron.sh
@@ -7,5 +7,6 @@ echo "`date` starting $1 $2" >> log.txt
 cd go
 git pull
 cd ..
+export PATH=$PATH:/usr/local/bin
 source <(./ec2 -profile $1 -gen $2) 2>&1 | tee -a log.txt
 echo "`date` done $1 $2" >> log.txt
